c4f7d194b995116fc8977e4e090ffd1601435e35
DeeB313 <116029548+DeeB313@users.noreply.github.com>
2025-03-17 20:51:10 -0500

    Update tests_bmi_calculator.py

--- tests/tests_bmi_calculator.py
+++ tests/tests_bmi_calculator.py
@@ -7,21 +7,38 @@ class TestBMICalculator(unittest.TestCase):
         bmi = calculate_bmi(100, 5, 5)  # 100 lbs, 5 feet 5 inches
         category = get_bmi_category(bmi)
         self.assertEqual(category, "Underweight")
+        print(f"Test 1: BMI = {bmi:.2f},  Expected Category: {category}")
 
-    def test_bmi_normal(self):
-        bmi = calculate_bmi(145, 5, 5)  # 150 lbs, 5 feet 5 inches
+    def test_bmi_normal_lower_bound(self):
+        bmi = calculate_bmi(109, 5, 5)  # 150 lbs, 5 feet 5 inches
         category = get_bmi_category(bmi)
         self.assertEqual(category, "Normal weight")
+        print(f"Test 2: BMI = {bmi:.2f},  Expected Category: {category}")
 
-    def test_bmi_overweight(self):
-        bmi = calculate_bmi(175, 5, 5)  # 200 lbs, 5 feet 5 inches
+    def test_bmi_normal_upper_bound(self):
+        bmi = calculate_bmi(146.7, 5, 5)  # 150 lbs, 5 feet 5 inches
+        category = get_bmi_category(bmi)
+        self.assertEqual(category, "Normal weight")
+        print(f"Test 3: BMI = {bmi:.2f},  Expected Category: {category}")
+
+
+    def test_bmi_overweight_lower_bound(self):
+        bmi = calculate_bmi(146.8, 5, 5)  # 200 lbs, 5 feet 5 inches
         category = get_bmi_category(bmi)
         self.assertEqual(category, "Overweight")
+        print(f"Test 4: BMI = {bmi:.2f},  Expected Category: {category}")
+    
+    def test_bmi_overweight_upper_bound(self):
+        bmi = calculate_bmi(176, 5, 5)  # 200 lbs, 5 feet 5 inches
+        category = get_bmi_category(bmi)
+        self.assertEqual(category, "Overweight")
+        print(f"Test 5: BMI = {bmi:.2f},  Expected Category: {category}")
 
     def test_bmi_obese(self):
-        bmi = calculate_bmi(250, 5, 5)  # 250 lbs, 5 feet 5 inches
+        bmi = calculate_bmi(200, 5, 5)  # 250 lbs, 5 feet 5 inches
         category = get_bmi_category(bmi)
         self.assertEqual(category, "Obese")
+        print(f"Test 6: BMI = {bmi:.2f},  Expected Category: {category}")
 
     def test_bmi_invalid_input_weight(self):
         # Testing for invalid input
